f7d27d3101da61658575ad33cde562b7fa86f230
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-05-12 09:56:49 -0500

    update time parsing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 const days = ['M', 'Tu', 'W', 'Th', 'F'];
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
 const Banner = ({ title }) => (
   <h1 className="title">{ title || '[loading...]' }</h1>
@@ -15,24 +16,22 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
 
-const daysOverlap = (course1, course2) => (
-  days.some(day => course1.days.includes(day) && course2.days.includes(day))
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
 );
 
-const inMinutes = (time) => (
-  (([hh, mm]) => hh * 60 + mm * 1) (time.split(':'))
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
 );
 
-const timesOverlap = (course1, course2) => (
-  Math.max(inMinutes(course1.start), inMinutes(course2.start))
-    < Math.min(inMinutes(course1.end), inMinutes(course2.end))
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
 );
 
 const courseConflict = (course1, course2) => (
   course1 !== course2
   && getCourseTerm(course1) === getCourseTerm(course2)
-  && daysOverlap(course1, course2)
-  && timesOverlap(course1, course2)
+  && timeConflict(course1, course2)
 );
 
 const hasConflict = (course, selected) => (
@@ -93,6 +92,27 @@ const CourseList = ({ courses }) => {
   );
 };
 
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
   const url = 'https://www.cs.northwestern.edu/academics/courses/394/data/cs-courses.php';
@@ -102,7 +122,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, [])
