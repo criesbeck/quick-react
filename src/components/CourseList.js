2107bd947cd8e8a69857824651ce8b97906c79e7
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-06-17 13:33:56 -0500

    move courselist to separate component

---
+++ src/components/CourseList.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import "rbx/index.css";
+import { Button } from "rbx";
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+)
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => course !== selection && courseConflict(course, selection))
+);
+
+const buttonState = selected => (
+  selected ? `success` : null
+)
+
+const TermSelector = ({ state }) => (
+  <Button.Group hasAddons>
+    {Object.values(terms)
+      .map(value =>
+        <Button key={value}
+          color={buttonState(value === state.term)}
+          onClick={() => state.setTerm(value)}
+        >
+          {value}
+        </Button>
+      )
+    }
+  </Button.Group>
+);
+  
+const Course = ({ course, state }) => (
+  <Button
+      color={ buttonState(state.selected.includes(course)) }
+      onClick={ () => state.toggle(course) }
+      disabled={ hasConflict(course, state.selected) }
+      >
+      { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  </Button>
+);
+
+const useSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
+  };
+  return [ selected, toggle ];
+};
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+  
+  return (
+    <React.Fragment>
+      <TermSelector state={ { term, setTerm } } />
+      <Button.Group>
+          { termCourses.map(course =>
+            <Course key={ course.id } course={ course } state={ { selected, toggle } } />) }
+      </Button.Group>
+    </React.Fragment>
+  );
+};
+
+export default CourseList;
